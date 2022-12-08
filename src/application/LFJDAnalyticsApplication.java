fdc23b620a35276970f1f61622d39ae868b719b6
joelflury <103413058+joelflury@users.noreply.github.com>
2022-12-08 22:34:31 +0100

    master-jf

--- src/application/LFJDAnalyticsApplication.java
+++ src/application/LFJDAnalyticsApplication.java
@@ -1,15 +1,18 @@
 package application;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class LFJDAnalyticsApplication  extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader homeLoader = new FXMLLoader(LFJDAnalyticsApplication.class.getResource("/view/Home-view.fxml"));
+        FXMLLoader homeLoader = new FXMLLoader(LFJDAnalyticsApplication.class.getResource("/view/Trend-view.fxml"));
         primaryStage.setScene(new Scene(homeLoader.load()));
+        primaryStage.getIcons().add(new Image("file:src/resources/img/logo6.png"));
 
 
 
